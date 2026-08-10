ba6b8d7976b6f47e912178ed8d030f3ed46c2ada
Sajin Reyans <sajin2917@gmail.com>
2026-08-10 13:55:40 +0530

    Create sorted squares.py

---
+++ arrays /sorted squares.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def sortedSquares(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        n=len(nums)
+        left=0
+        right=n-1
+        pos=n-1
+        result=[0]*n
+        while(left<=right):
+            if abs(nums[left])>abs(nums[right]):
+                result[pos]=nums[left]*nums[left]
+                left+=1
+            else:
+                result[pos]=nums[right]*nums[right]
+                right-=1
+            pos-=1
+        return result  
