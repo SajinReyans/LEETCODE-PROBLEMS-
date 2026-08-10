873b20c10d8a25733808d2679453ebfc0e55f04c
Sajin Reyans <sajin2917@gmail.com>
2026-08-10 23:25:28 +0530

    Create 1475. Final Prices With a Special Discount in a Shop.py

---
+++ stack/1475. Final Prices With a Special Discount in a Shop.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def finalPrices(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: List[int]
+        """
+        result=prices[:]
+        n=len(prices)
+        for i in range(n):
+            for j in range(i+1,n):
+                if prices[j]<=prices[i]:
+                    result[i]-=prices[j]
+                    break
+        return result
