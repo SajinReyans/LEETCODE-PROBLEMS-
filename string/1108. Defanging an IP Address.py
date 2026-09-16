fdfd9011315e6544ea7685152c9fbf468f6b40dd
Sajin Reyans <sajin2917@gmail.com>
2026-09-16 23:15:50 +0530

    Create 1108. Defanging an IP Address.py

---
+++ string/1108. Defanging an IP Address.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def defangIPaddr(self, address):
+        """
+        :type address: str
+        :rtype: str
+        """
+        result=""
+        for i in range(len(address)):
+            if address[i]==".":
+                result+="[.]"
+            else:
+                result+=address[i]
+        return result
